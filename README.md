0517fce6e982e6b653ce8c6dbb087d1fca150b6b
Wilson Tai <taiwilson5@gmail.com>
2023-11-11 20:23:34 +0800

    pattern gen

---
+++ README.md
@@ -0,0 +1,52 @@
+# Reaction-Diffusion Simulation README
+
+## Overview
+
+This Python script simulates a reaction-diffusion system, a fundamental concept in physical chemistry and pattern formation. It uses numpy for numerical computations, matplotlib for visualization, scipy for convolution operations, and click for command-line interface functionality. The simulation generates patterns based on user-defined parameters and saves the results as images.
+
+## Features
+
+- **Reaction-Diffusion Simulation**: Models the interaction between two chemical substances.
+- **Image Generation**: Creates visual representations of the reaction-diffusion process.
+- **Interactive Parameters**: Allows customization of simulation parameters.
+- **Batch Image Generation**: Generates and saves images for a range of simulation steps.
+
+## Installation
+
+Before running the script, ensure you have Python installed on your system along with the following packages:
+- numpy
+- matplotlib
+- scipy
+- click
+
+You can install these packages using pip:
+```bash
+pip install numpy matplotlib scipy click
+```
+
+## Usage
+
+The script can be used in two main ways:
+
+1. **As a Command-Line Tool**: 
+
+   Run the script from the command line with optional arguments to specify the range and interval of simulation steps.
+
+   ```bash
+   python pattern.py --min_step [MIN_STEPS] --max_step [MAX_STEPS] --interval [INTERVAL]
+   ```
+
+## Functions
+
+- `reaction_diffusion(a, b, da, db, feed, k, steps)`: Performs the reaction-diffusion process.
+- `interactive_simulation_steps(steps, da, db, feed, k, colormap)`: Runs the interactive simulation for a given number of steps.
+- `steps_generator(min_step, max_step, interval)`: Generates images for a range of steps at specified intervals.
+
+## Parameters
+
+- `a`, `b`: Numpy arrays representing the chemical substances.
+- `da`, `db`: Diffusion rates of substances `a` and `b`.
+- `feed`: Feed rate.
+- `k`: Kill rate.
+- `steps`: Number of simulation steps.
+- `colormap`: Colormap for the resulting image (in `interactive_simulation_steps`).
---
+++ pattern.py
@@ -0,0 +1,136 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import convolve
+import os
+from typing import Tuple
+import click
+
+# Constants
+FILE_PATH = "pattern"
+
+
+def reaction_diffusion(
+    a: np.ndarray,
+    b: np.ndarray,
+    da: float,
+    db: float,
+    feed: float,
+    k: float,
+    steps: int,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Performs the reaction-diffusion process.
+
+    Args:
+      a: The 'A' component of the system.
+      b: The 'B' component of the system.
+      da: Diffusion rate of 'A'.
+      db: Diffusion rate of 'B'.
+      feed: Feed rate.
+      k: Kill rate.
+      steps: Number of simulation steps.
+
+    Returns:
+      Tuple of numpy arrays representing the final state of 'A' and 'B'.
+    """
+    kernel = np.array([[0.05, 0.2, 0.05], [0.2, -1, 0.2], [0.05, 0.2, 0.05]])
+
+    for _ in range(steps):
+        la = convolve(a, kernel, mode="wrap")
+        lb = convolve(b, kernel, mode="wrap")
+        ab2 = a * b**2
+        a += da * la - ab2 + feed * (1 - a)
+        b += db * lb + ab2 - (k + feed) * b
+        np.clip(a, 0, 1, out=a)  # Ensuring values are within [0, 1]
+        np.clip(b, 0, 1, out=b)
+
+    return a, b
+
+
+def interactive_simulation_steps(
+    steps: int,
+    da: float = 1.0,
+    db: float = 0.5,
+    feed: float = 0.055,
+    k: float = 0.062,
+    colormap: str = "magma",
+) -> plt.Figure:
+    """Runs the interactive simulation for a given number of steps.
+
+    Args:
+      steps: Number of simulation steps.
+      da, db, feed, k: Parameters for the reaction-diffusion process.
+      colormap: Colormap for the resulting image.
+
+    Returns:
+      Matplotlib figure object.
+    """
+    size = 100
+    a = np.ones((size, size))
+    b = np.zeros((size, size))
+    np.random.seed(42)
+    b[
+        size // 2 - 10 : size // 2 + 10, size // 2 - 10 : size // 2 + 10
+    ] = np.random.rand(20, 20)
+    a, b = reaction_diffusion(a, b, da, db, feed, k, steps)
+
+    fig = plt.figure()
+    plt.imshow(b, cmap=colormap, interpolation="bilinear")
+    plt.axis("off")
+    return fig
+
+
+def steps_generator(min_step: int, max_step: int, interval: int = 500):
+    """Generates images for a range of steps at specified intervals.
+
+    Args:
+      min_step: Minimum number of steps.
+      max_step: Maximum number of steps.
+      interval: Interval between steps.
+    """
+
+    folder_name = f"{min_step}_{max_step}_{interval}_pattern"
+    print(f"Generating pattern to {folder_name} ...")
+
+    directory_path = os.path.join(FILE_PATH, folder_name)
+    os.makedirs(directory_path, exist_ok=True)
+
+    for i in range(min_step, max_step, interval):
+        print(f"image {i} generating ...")
+        img = interactive_simulation_steps(i)
+        file_name = os.path.join(directory_path, f"{i}.png")
+        img.savefig(file_name)
+        print(f"image {i} saved ...")
+
+
+@click.command()
+@click.option(
+    "--min_step",
+    default=5000,
+    type=int,
+    help="Minimum number of steps.",
+)
+@click.option(
+    "--max_step",
+    default=6000,
+    type=int,
+    help="Maximum number of steps.",
+)
+@click.option(
+    "--interval",
+    default=500,
+    type=int,
+    help="Interval between steps.",
+)
+def main(min_step: int, max_step: int, interval: int):
+    """Command-line tool to generate reaction-diffusion pattern images.
+
+    Args:
+      min_step: Minimum number of steps for the simulation.
+      max_step: Maximum number of steps for the simulation.
+      interval: Interval between steps for generating images.
+    """
+    steps_generator(min_step, max_step, interval)
+
+
+if __name__ == "__main__":
+    main()
